0f2900b8f93dc7da3d51d8e333e81861d6fb7211
root <tsegayealx@gmail.com>
2022-03-10 11:05:55 -0800

    me

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,19 +1,19 @@
-#include<stdio.h>
-/*
+#include <stdio.h>
+
+/**
  *
- *  * main - Entry point
+   * Description: main - Prints out a string to stdout.
  *
- *   *
+ *   * Return: 0 if success.
  *
- *    * Return: Always 0 (Success)
- *
-      */
+     */
+
 int main(void)
 
 {
 
-		puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 
-			return (0);
+	return (0);
 
 }
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
-/**
+/*
  *
- *  * Description: main - Prints out a string to stdout.
+  * Description: main - Prints out a string to stdout.
  *
  *   * Return: 0 if success.
  *
- *    */
+     */
 
 int main(void)
 
