ba88f2885f90fd3d0835651661964ec4fee0e2c6
tsegacmd <99798780+tsegacmd@users.noreply.github.com>
2022-03-12 04:46:45 +0300

    Update 9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -2,9 +2,7 @@
  * File: 9-print_comb.c
  * Auth: tsegazabe
  */
-
 #include <stdio.h>
-
 /**
  * main - Prints all possible combinations of single-digit numbers.
  *
@@ -19,12 +17,10 @@ int main(void)
 		putchar((num % 10) + '0');
 		if (num == 9)
 			continue;
-		
 		putchar(',');
 		putchar(' ');
 	}
 
 	putchar('\n');
-
 	return (0);
 }
