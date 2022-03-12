fdb9704bde0262b42bee7666463f190a35a6496e
tsegacmd <99798780+tsegacmd@users.noreply.github.com>
2022-03-12 04:29:55 +0300

    Update 0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -17,7 +17,7 @@ int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n > 0)
-	printf("%d is posetive\n",n);
+	printf("%d is posetive\n", n);
 else if (n < 0)
 	printf("%d is negative\n", n);
 else
