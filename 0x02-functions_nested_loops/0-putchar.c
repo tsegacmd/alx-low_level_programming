7e32d234334796d42497dd7d354d17e29d44d5b6
tsegacmd <99798780+tsegacmd@users.noreply.github.com>
2022-03-15 20:29:04 +0300

    Create 0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,26 @@
+/*
+ * File: 0-putchar.c
+ * Auth: tsega
+ *
+ */
+
+#include "_putchar.h"
+
+/**
+ * main - Prints "_putchar" followed by a new line.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+  
+	return (0);
+}
