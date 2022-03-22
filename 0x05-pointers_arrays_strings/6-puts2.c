6fcaf3c44bcd6084d710d07b460aa0a95d5338f9
tsegacmd <99798780+tsegacmd@users.noreply.github.com>
2022-03-22 22:14:06 +0300

    Update 6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,24 +1,22 @@
 #include "main.c"
 
 /**
- * puts2 - prints one char out of 2
- * @str: type
- *
+ * puts2 - print  every other character of a string
+ * @str:pointer char
+ * return:void
  */
-
 void puts2(char *str)
 {
-	int i, j = 0;
-
-	while (str[i] != '\0')
+	int i, x;
+i = 0;
+x = 0;
+	while (str[i++])
 	{
-		i++;
+		x++;
 	}
-
-	while (j < i)
-	{
-		_putchar(str[j]);
-		j = j + 2;
-	}
-	_putchar('\n');
+for (i = 0; i < x; i += 2)
+{
+_putchar(*(str + i));
+}
+_putchar('\n');
 }
