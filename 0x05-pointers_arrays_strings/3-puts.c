0c3f257589c51c75924637f75996a0a0b022c509
tsegacmd <99798780+tsegacmd@users.noreply.github.com>
2022-03-22 21:56:57 +0300

    Create 3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.c"
+/**
+ * _puts - prints string
+ * @str: input string
+ * Return: no return
+ */
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str++;
+	}
+	_putchar('\n');
+}
