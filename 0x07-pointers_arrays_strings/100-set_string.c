ac1ca0a866d5cc68cb90297671a0ef34b756c7ed
tsegacmd <99798780+tsegacmd@users.noreply.github.com>
2022-03-29 20:44:23 +0300

    Update 100-set_string.c

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -1,16 +1,12 @@
-#include "main.c"
-#include <stdio.h>
+#include "main.h"
 
 /**
- * set_string - sets the value of a pointer to a char
- *
- * @s: Double pointer
- *
- * @to: Pointer
- *
- * Return: Void
- */
-
+  * set_string - Sets the value of a pointer to char
+  * @s: the value to modify
+  * @to: the value to assign
+  *
+  * Return: Nothing.
+  */
 void set_string(char **s, char *to)
 {
 	*s = to;
