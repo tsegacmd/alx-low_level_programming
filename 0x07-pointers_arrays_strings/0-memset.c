e611f953d8ccdedf23c6bbb466fb34263c38e254
tsegacmd <99798780+tsegacmd@users.noreply.github.com>
2022-03-29 20:22:53 +0300

    Create 0-memset.c

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.c"
+
+/**
+ * _memset -me
+ * @s: pointer to char params
+ * @b: data to change
+ * @n: index
+ * Return: *s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
