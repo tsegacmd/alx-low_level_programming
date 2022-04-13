c9e0ca079b9a8e8d523c81b7576d2aecec9a9b9f
tsegacmd <99798780+tsegacmd@users.noreply.github.com>
2022-04-13 22:28:13 +0300

    Update 0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,15 +1,12 @@
-#include "function_pointers.c"
-
 /**
- * print_name - prints a name
- * @name: name's main
- * @f: void function
- */
-
+  *print_name - prints a name.
+  *@name: pointer to name.
+  *@f: function pointer.
+  *
+  *Return: void.
+  */
 void print_name(char *name, void (*f)(char *))
 {
 	if (name && f)
-	{
 		f(name);
-	}
 }
