d6d2e8559c782705d815595ca43dfa8eb60ff2a2
tsegacmd <99798780+tsegacmd@users.noreply.github.com>
2022-04-13 22:21:50 +0300

    Update 2-int_index.c

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,28 +1,38 @@
-#include "function_pointers.c"
-
 /**
- * int_index - searches for an integer
- * @array: array of elemnts
- * @size: number of elements in the array
- * @cmp: is a pointer to the function to be used to compare values
- * Return: index first element cmp function does not return 0
- * If no element matches, return -1
- * If size <= 0, return -1
- */
-
+  *int_index - searches for an integer.
+  *@array: array of integers.
+  *@size: number of elements in array.
+  *@cmp: pointer to function used to compare values.
+  *
+  *Return: index of first element.
+  *-1 if no element is found or siz <= 0.
+  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	int i;
+	int i, res;
 
+	res = -1;
 	if (array && cmp)
 	{
+
+		if (size <= 0)
+		{
+			return (res);
+		}
 		for (i = 0; i < size; i++)
 		{
-			if (cmp(array[i]) != 0)
+			cmp(array[i]);
+			if (cmp(array[i]) > 0)
 			{
-				return (i);
+				res = i;
+				break;
+			}
+			if ((cmp(array[i]) == (-1)))
+			{
+				return (res);
 			}
 		}
+
 	}
-	return (-1);
+	return (res);
 }
