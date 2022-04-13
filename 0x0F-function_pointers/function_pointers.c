b90543906401eb066ae24158fe0e8d5a2ce8e7ec
tsegacmd <99798780+tsegacmd@users.noreply.github.com>
2022-04-13 22:27:05 +0300

    Update function_pointers.c

--- 0x0F-function_pointers/function_pointers.c
+++ 0x0F-function_pointers/function_pointers.c
@@ -1,12 +1,9 @@
-#ifndef FILE_F_POINTRS
-#define FILE_F_POINTRS
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
 
-#include <stdlib.h>
-
+void _putchar(char *c);
 void print_name(char *name, void (*f)(char *));
-
 void array_iterator(int *array, size_t size, void (*action)(int));
-
 int int_index(int *array, int size, int (*cmp)(int));
 
-#endif
+#endif /*FUNCTION_POINTERS_H*/
