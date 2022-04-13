730f24fbb867770dcc9cd5ec5d2f73cf9dba82ef
tsegacmd <99798780+tsegacmd@users.noreply.github.com>
2022-04-13 22:19:46 +0300

    Update 1-array_iterator.c

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,22 +1,21 @@
 #include <stdio.h>
-
 /**
- * array_iterator - executes funct
- * @array: array of elements
- * @size: array's size
- * @action: pointer to function
- * Return: No
- */
-
+  *array_iterator - function that executes function given as param.
+  *@array: array of elements.
+  *@size: size of array.
+  *@action: function pointer.
+  *
+  *Return: void.
+  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int i;
 
-	if (array && size && action)
+	if (array && size > 0 && action)
 	{
 		for (i = 0; i < size; i++)
 		{
-			(*action)(array[i]);
+			action(array[i]);
 		}
 	}
 }
