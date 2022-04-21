b8e192ed49c39eb7091853803a5bc981e34c581f
tsegacmd <99798780+tsegacmd@users.noreply.github.com>
2022-04-21 22:53:34 +0300

    Update 2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,19 +1,27 @@
+#ifndef LISTS_H
+#define LISTS_H
 #include <stdlib.h>
-#include "lists.h"
-
+#include <string.h>
 /**
- * free_list - frees a linked list
- * @head: list_t list to be freed
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * for Holberton project
  */
-void free_list(list_t *head)
+typedef struct list_s
 {
-	list_t *tmp;
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
 
-	while (head)
-	{
-		tmp = head->next;
-		free(head->str);
-		free(head);
-		head = tmp;
-	}
-}
+size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
+void free_list(list_t *head);
+
+#endif
