611c788a8080575ce4613061cd79d3d54ccec441
tsegacmd <99798780+tsegacmd@users.noreply.github.com>
2022-04-25 21:15:10 +0300

    Update 9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,48 +1,50 @@
+#include <stdlib.h>
+#include <stdio.h>
 #include "lists.h"
 
 /**
- * insert_nodeint_at_index - inserts a new node at a given position.
- * @head: head of linked list
- * @idx: index to insert the node
- * @n: new node data
- * Return: new node
- */
-
+  * insert_nodeint_at_index - ...
+  * @head: ...
+  * @idx: ...
+  * @n: ...
+  *
+  * Return: ...
+  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *current;
-	listint_t *new;
+	listint_t *new_node, *iter;
+	unsigned int count = 1;
 
-	if (head == NULL)
-		return (0);
-	new = malloc(sizeof(listint_t));
-	if (!new)
-		return (0);
-
-	new->next = NULL;
-	new->n = n;
-
-	if (idx == 0)
+	if (head)
 	{
-		new->next = *head;
-		(*head) = new;
-		return (new);
-	}
-
-	current = *head;
-
-	while (idx != 1)
-	{
-		current = current->next;
-		--idx;
-		if (current == NULL)
-		{
-			free(new);
+		new_node = malloc(sizeof(listint_t));
+		if (new_node == NULL)
 			return (NULL);
-		}
-	}
-	new->next = current->next;
-	current->next = new;
 
-	return (new);
+		new_node->n = n;
+		if (idx > 0)
+		{
+			iter = *head;
+			while (iter)
+			{
+				if (count == idx)
+				{
+					new_node->next = iter->next;
+					iter->next = new_node;
+					return (new_node);
+				}
+				iter = iter->next;
+				count++;
+			}
+			if (idx > count)
+				return (NULL);
+		}
+		else
+		{
+			new_node->next = *head;
+			*head = new_node;
+		}
+		return (new_node);
+	}
+	return (NULL);
 }
