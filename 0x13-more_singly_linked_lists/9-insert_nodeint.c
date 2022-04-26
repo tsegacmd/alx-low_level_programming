f45e24508e66a0fadfb69a6dcb78ad40a0565dc3
tsegacmd <99798780+tsegacmd@users.noreply.github.com>
2022-04-26 23:38:43 +0300

    Update 9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,71 +1,51 @@
-#include <stdlib.h>
 #include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
 /**
- * insert_nodeint_at_index - inserts a new node at a given position.
- * @head: Double pointer to the actual position of the linked list (struct)
- * @idx: Position for the new node.
- * @n: Number to be passed to the new node
- *
- * Return: Sum of the value of all nodes.
+ *insert_nodeint_at_index - inserts a node at a certain index
+ *@head:pointer to listint
+ *@idx:index of list to add new node
+ *@n:new node data
+ *Return:pointer to the new node
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
+	listint_t *new, *node;
 	unsigned int i = 0;
-	listint_t *new;
-	listint_t *tmp = *head;
+	listint_t *temp;
 
 	if (head == NULL)
-		return (NULL);
-
-	if (idx == 0)
 	{
-		tmp = add_nodeint(head, n);
-		return (tmp);
+		return (NULL);
 	}
-
-	while (i < idx)
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
 	{
-		tmp = tmp->next;
-		if (tmp == NULL)
-			return (NULL);
-		i++;
+		free(new);
+		return (NULL);
 	}
-
-	new = malloc(sizeof(listint_t));
-
-	if (new == NULL)
-		return (NULL);
-
-	new->n = tmp->n;
-	new->next = tmp->next;
-	tmp->n = n;
-	tmp->next = new;
-
-	return (tmp);
-}
-
-/**
- * add_nodeint - adds a new node at the beginning of a list_t list.
- * @head: Pointer to the actual position of the linked list (struct)
- * @n: Value to include in the new node
- *
- * Return: Size of the linked_list.
- */
-listint_t *add_nodeint(listint_t **head, const int n)
-{
-	listint_t *new;
-
-	if (head == NULL)
-		return (NULL);
-
-	new = malloc(sizeof(listint_t));
-
-	if (new == NULL)
-		return (NULL);
-
 	new->n = n;
-	new->next = *head;
-	*head = new;
-
-	return (*head);
+	new->next = NULL;
+	if (i == idx)
+	{
+		new->next = *head;
+		*head = new;
+		return (new);
+	}
+	temp = *head;
+	i = 0;
+	while (temp->next && i < (idx - 1))
+	{
+		i++;
+		temp = temp->next;
+	}
+	if (idx > (i + 1))
+	{
+		free(new);
+		return (NULL);
+	}
+	node = temp->next;
+	temp->next = new;
+	new->next = node;
+	return (new);
 }
