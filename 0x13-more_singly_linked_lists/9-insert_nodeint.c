fba71db62156bf6d8c03c77af6bd3dd6fca443b3
tsegacmd <99798780+tsegacmd@users.noreply.github.com>
2022-04-26 23:09:23 +0300

    Update 9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,48 +1,71 @@
+#include <stdlib.h>
 #include "lists.h"
-
 /**
  * insert_nodeint_at_index - inserts a new node at a given position.
- * @head: head of linked list
- * @idx: index to insert the node
- * @n: new node data
- * Return: new node
+ * @head: Double pointer to the actual position of the linked list (struct)
+ * @idx: Position for the new node.
+ * @n: Number to be passed to the new node
+ *
+ * Return: Sum of the value of all nodes.
  */
-
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *current;
+	unsigned int i = 0;
 	listint_t *new;
+	listint_t *tmp = *head;
 
 	if (head == NULL)
-		return (0);
-	new = malloc(sizeof(listint_t));
-	if (!new)
-		return (0);
-
-	new->next = NULL;
-	new->n = n;
+		return (NULL);
 
 	if (idx == 0)
 	{
-		new->next = *head;
-		(*head) = new;
-		return (new);
+		tmp = add_nodeint(head, n);
+		return (tmp);
 	}
 
-	current = *head;
-
-	while (idx != 1)
+	while (i < idx)
 	{
-		current = current->next;
-		--idx;
-		if (current == NULL)
-		{
-			free(new);
+		tmp = tmp->next;
+		if (tmp == NULL)
 			return (NULL);
-		}
+		i++;
 	}
-	new->next = current->next;
-	current->next = new;
 
-	return (new);
+	new = malloc(sizeof(listint_t));
+
+	if (new == NULL)
+		return (NULL);
+
+	new->n = tmp->n;
+	new->next = tmp->next;
+	tmp->n = n;
+	tmp->next = new;
+
+	return (tmp);
+}
+
+/**
+ * add_nodeint - adds a new node at the beginning of a list_t list.
+ * @head: Pointer to the actual position of the linked list (struct)
+ * @n: Value to include in the new node
+ *
+ * Return: Size of the linked_list.
+ */
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *new;
+
+	if (head == NULL)
+		return (NULL);
+
+	new = malloc(sizeof(listint_t));
+
+	if (new == NULL)
+		return (NULL);
+
+	new->n = n;
+	new->next = *head;
+	*head = new;
+
+	return (*head);
 }
